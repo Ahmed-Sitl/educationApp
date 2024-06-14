baea3970ed1cdf809293ebefa41d61dae717112a
Ahmed Nabil <bplahmed28d@gmail.com>
2024-06-14 17:33:36 +0300

    Login Design without functionality

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import Colors from "./App/Shared/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar hidden={true} />
+      <Image
+        source={require("./App/Assets/Images/login.png")}
+        style={styles.image}
+      />
+
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to CodeBox</Text>
+        <Text style={styles.login}>Login & Signup</Text>
+        <TouchableOpacity style={styles.button}>
+          <Ionicons
+            style={{ marginRight: 10 }}
+            name="logo-google"
+            size={24}
+            color="white"
+          />
+          <Text style={{ color: "#fff" }}>Sign In with Google</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: "45%",
+    // marginTop: 35,
+    // resizeMode: "conver",
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 35,
+    marginTop: -25,
+    backgroundColor: "#fff",
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  login: {
+    textAlign: "center",
+    marginTop: 20,
+    fontSize: 20,
+    // paddingTop: 40,
+  },
+  button: {
+    backgroundColor: Colors.primary,
+    padding: 10,
+    margin: 30,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    borderRadius: 10,
   },
 });
---
+++ App/Shared/Colors.js
@@ -0,0 +1,1 @@
+export default Colors = { primary: "#0C7DE4" };
